a770e241377987bc4d3db38941177553122d4814
emiliabrgf <burggrafemilia@gmail.com>
2021-03-23 10:17:36 +0100

    refac obstacle: deleted unused methods

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -4,42 +4,30 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-// Start is called before the first frame update
-void Start()
-{
-        
-}
-
-// Update is called once per frame
-void Update()
-{
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
         //if obstacle's position x is < -15f it will be destroyed
-        if (transform.position.x > 15f)
+        if(transform.position.x < -15f)
         {
             Destroy(gameObject);
         }
+        //if obstacle's position x is < -15f it will be destroyed
+        if (transform.position.x > 15f)
+        {
+             Destroy(gameObject);
+        }
 
     }
-
-
-
-private void FixedUpdate()
-{
-
-    rb.velocity = Vector2.left * MoveSpeed;
-
-}
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left * MoveSpeed;
+    }
 }
